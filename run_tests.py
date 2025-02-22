85cb1e80beddab3807ada0e8b14d8053076ffe83
Bosho M <kipbrio@gmail.com>
2025-02-22 16:57:53 +0200

    deleted unused files

--- run_tests.py
+++
@@ -1,8 +0,0 @@
-from federated_learning import load_configuration, main_experiment
-
-if __name__ == "__main__":
-    data_loading_option = "processed_data"  # Options: "processed_data", "processed_data_with_hop", "feature_prop"
-    model_type = "GCN"  # Options: "GCN", "GAT"
-    
-    clients_num, beta, cfg = load_configuration()
-    main_experiment(clients_num, beta, data_loading_option, model_type, cfg)
